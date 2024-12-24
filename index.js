f1170308f51992306cc9c7b465fa51bb213d1406
therajesh8769 <118832704+therajesh8769@users.noreply.github.com>
2024-12-24 20:49:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,8 @@ app.get('/api/registration', async (req, res) => {
     }
   });
 
-app.listen(8080, () => {
-    console.log("listening on port 8080");
-});
+// app.listen(8080, () => {
+//     console.log("listening on port 8080");
+// });
+module.exports = app;
 
