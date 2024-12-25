63d4de5d580db9bf1a43871c11ae178057ee5839
Rajesh <therajesh8769@gmail.com>
2024-12-25 13:06:46 +0530

    updated cors

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function main() {
 main();
 
 app.use(cors({
-    origin: "https://abhiyantrix-frontend.vercel.app/", // Allow requests from your frontend
+    origin: ["https://abhiyantrix-frontend.vercel.app",'*'], // Allow requests from your frontend
     methods: ["GET", "POST", "PATCH", "DELETE"], // Include PATCH and DELETE for event operations
     credentials: true               // Include credentials if necessary
 }));
